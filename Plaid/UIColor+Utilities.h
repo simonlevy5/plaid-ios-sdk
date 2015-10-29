be9f22960871674d168ac8f91750b298df4daf65
Simon Levy <simon@vouch.com>
2015-10-29 10:27:10 -0700

    Document and fix UIColor category

--- Plaid/UIColor+Utilities.h
+++ Plaid/UIColor+Utilities.h
@@ -9,8 +9,37 @@
 
 @interface UIColor (Utilities)
 
-+ (UIColor *)colorWithHex:(NSString*)hex alpha:(CGFloat)alpha;
+/**
+ Create a UIColor with a hexadecimal string, such as #00AAFF.
+ 
+ @param hex Hexadecimal string representing a color.
+ @param alpha The alpha value to use when creating this color.
+ 
+ @return UIColor object representing the hexadecimal string.
+ */
++ (UIColor *)colorWithHex:(NSString *)hex alpha:(CGFloat)alpha;
+
+/**
+ Create a UIColor with 8-bit red, green, and blue values and an alpha value.
+ 
+ @param red The 8-bit red value.
+ @param green The 8-bit green value.
+ @param blue The 8-bit blue value.
+ @param alpha The alpha value for this color.
+ 
+ @return UIColor representing the input 8-bit color values.
+ */
 + (UIColor *)colorWithRgbaRed:(float)red green:(float)green blue:(float)blue alpha:(float)alpha;
+
+/**
+ Create a UIColor with 8-bit red, green, and blue values and an alpha value.
+ 
+ @param red The 8-bit red value.
+ @param green The 8-bit green value.
+ @param blue The 8-bit blue value.
+
+ @return UIColor representing the input 8-bit color values.
+ */
 + (UIColor *)colorWithRgbaRed:(int)red green:(int)green blue:(int)blue;
 
 @end
--- Plaid/UIColor+Utilities.m
+++ Plaid/UIColor+Utilities.m
@@ -30,11 +30,11 @@
 }
 
 + (UIColor *)colorWithRgbaRed:(float)red green:(float)green blue:(float)blue alpha:(float)alpha {
-    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+    return [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:alpha];
 }
 
 + (UIColor *)colorWithRgbaRed:(int)red green:(int)green blue:(int)blue {
-  return [self colorWithRgbaRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1];
+  return [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1];
 }
 
 @end
