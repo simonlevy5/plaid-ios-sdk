755af7a6eceb23e0f112207638817cee9a5bc4df
Simon Levy <simon@vouch.com>
2015-10-29 11:09:51 -0700

    Add header to make swift integration easier

---
+++ Plaid/Plaid-Swift.h
@@ -0,0 +1,17 @@
+//
+//  Plaid-Swift.h
+//  Plaid
+//
+//  Created by Simon Levy on 10/29/15.
+//
+
+#import "Plaid.h"
+#import "PLDAccount.h"
+#import "PLDAuthentication.h"
+#import "PLDAuthResponse.h"
+#import "PLDConnectResponse.h"
+#import "PLDCategory.h"
+#import "PLDDefines.h"
+#import "PLDInstitution.h"
+#import "PLDLongTailInstitution.h"
+#import "PLDTransaction.h"
