592594bc00de05c2d0d4e3a45c28ca9a5e0176c9
Simon Levy <simon@vouch.com>
2016-01-08 08:07:27 -0800

    Removing APIs that use client token/secret
    Also cleaning up associated tests

--- Tests/ios/PlaidTests.m
+++ Tests/ios/PlaidTests.m
@@ -33,7 +33,6 @@ static NSString *const kTestPin = @"1234";
   [super setUp];
 
   _plaid = [Plaid sharedInstance];
-  [_plaid setClientId:@"test_id" secret:@"test_secret"];
   [_plaid setPublicKey:@"test_key"];
 }
 
@@ -42,9 +41,7 @@ static NSString *const kTestPin = @"1234";
 }
 
 - (void)testSettingNilAuthValues {
-  XCTAssertThrows([_plaid setClientId:nil secret:nil]);
-  XCTAssertThrows([_plaid setClientId:@"" secret:nil]);
-  XCTAssertThrows([_plaid setClientId:nil secret:@""]);
+  XCTAssertThrows([_plaid setPublicKey:nil]);
 }
 
 - (void)testLinkAddUserSuccess {
@@ -302,182 +299,6 @@ static NSString *const kTestPin = @"1234";
   [self waitForTestExpectations];
 }
 
-- (void)testAddAuthUser {
-  [_plaid setMockedResponse:[_plaid exampleAuthResponse]];
-
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid addAuthUserWithUsername:kTestUsername
-                         password:kTestPassword
-                             type:kTestType
-                          options:nil
-                       completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                        strongify(self);
-                        XCTAssertNil(error);
-                        XCTAssertNotNil(authentication);
-                        XCTAssertEqual([response class], [PLDAuthResponse class]);
-                        XCTAssertGreaterThan([[response accounts] count], 0);
-                        [expectation fulfill];
-  }];
-  [self waitForTestExpectations];
-}
-
-- (void)testStepAuthUser {
-  [_plaid setMockedResponse:[_plaid exampleAuthResponse]];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid stepAuthUserWithAccessToken:kTestAccessToken
-                          mfaResponse:@"test"
-                              options:nil
-                           completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                             strongify(self);
-                             XCTAssertNil(error);
-                             XCTAssertNotNil(authentication);
-                             XCTAssertEqual([response class], [PLDAuthResponse class]);
-                             XCTAssertGreaterThan([[response accounts] count], 0);
-                             [expectation fulfill];
-                           }];
-  [self waitForTestExpectations];
-}
-
-- (void)testGetAuthUser {
-  [_plaid setMockedResponse:[_plaid exampleAuthResponse]];
-
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid getAuthUserWithAccessToken:kTestAccessToken completion:^(id response, NSError *error) {
-    strongify(self);
-    XCTAssertNotNil(response);
-    XCTAssertGreaterThan([[response accounts] count], 0);
-    [expectation fulfill];
-  }];
-  [self waitForTestExpectations];
-}
-
-- (void)testPatchAuthUser {
-  [_plaid setMockedResponse:[_plaid exampleAuthResponse]];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid patchAuthUserWithAccessToken:kTestAccessToken
-                              username:kTestUsername
-                              password:kTestPassword
-                            completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                              strongify(self);
-                              XCTAssertNotNil(response);
-                              XCTAssertNotNil(authentication);
-                              XCTAssertGreaterThan([[response accounts] count], 0);
-                              [expectation fulfill];
-  }];
-  [self waitForTestExpectations];
-}
-
-- (void)testDeleteAuthUser {
-  [_plaid setMockedResponse:@{}];
-
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid deleteAuthUserWithAccessToken:kTestAccessToken completion:^(id response, NSError *error) {
-    strongify(self);
-    XCTAssertNotNil(response);
-    [expectation fulfill];
-  }];
-  [self waitForTestExpectations];
-}
-
-- (void)testAddConnectUser {
-  [_plaid setMockedResponse:[_plaid exampleConnectResponse]];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid addConnectUserWithUsername:kTestUsername
-                            password:kTestPassword
-                                type:kTestType
-                             options:nil
-                          completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                            strongify(self);
-                            XCTAssertNotNil(response);
-                            XCTAssertNotNil(authentication);
-                            XCTAssertGreaterThan([[response accounts] count], 0);
-                            XCTAssertGreaterThan([[response transactions] count], 0);
-                            [expectation fulfill];
-  }];
-  [self waitForTestExpectations];
-}
-
-- (void)testGetConnectUser {
-  [_plaid setMockedResponse:[_plaid exampleConnectResponse]];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid addConnectUserWithUsername:kTestUsername
-                            password:kTestPassword
-                                type:kTestType
-                             options:nil
-                          completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                            strongify(self);
-                            XCTAssertNotNil(response);
-                            XCTAssertNotNil(authentication);
-                            XCTAssertGreaterThan([[response accounts] count], 0);
-                            XCTAssertGreaterThan([[response transactions] count], 0);
-                            [expectation fulfill];
-                          }];
-  [self waitForTestExpectations];
-}
-
-- (void)testStepConnectUser {
-  [_plaid setMockedResponse:[_plaid exampleConnectResponse]];
-
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid stepConnectUserWithAccessToken:kTestAccessToken
-                             mfaResponse:@"test"
-                                 options:nil
-                              completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                                strongify(self);
-                                XCTAssertNotNil(response);
-                                XCTAssertNotNil(authentication);
-                                XCTAssertGreaterThan([[response accounts] count], 0);
-                                XCTAssertGreaterThan([[response transactions] count], 0);
-                                [expectation fulfill];
-                              }];
-  [self waitForTestExpectations];
-}
-
-- (void)testPatchConnectUser {
-  [_plaid setMockedResponse:[_plaid exampleConnectResponse]];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid patchConnectUserWithAccessToken:kTestAccessToken
-                                 username:kTestUsername
-                                 password:kTestPassword
-                               completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                                 strongify(self);
-                                 XCTAssertNotNil(response);
-                                 XCTAssertNotNil(authentication);
-                                 XCTAssertGreaterThan([[response accounts] count], 0);
-                                 XCTAssertGreaterThan([[response transactions] count], 0);
-                                 [expectation fulfill];
-                          }];
-  [self waitForTestExpectations];
-}
-
-- (void)testDeleteConnectUser {
-  [_plaid setMockedResponse:@{}];
-
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid deleteAuthUserWithAccessToken:kTestAccessToken
-                             completion:^(id response, NSError *error) {
-                               strongify(self);
-                               XCTAssertNotNil(response);
-                               [expectation fulfill];
-                          }];
-  [self waitForTestExpectations];
-}
-
 - (void)testGetCategoryById {
   [_plaid setMockedResponse:[_plaid randomCategoryObject]];
   
@@ -597,51 +418,6 @@ static NSString *const kTestPin = @"1234";
     XCTAssertGreaterThan([response count], 0);
     [expectation fulfill];
   }];
-  [self waitForTestExpectations];
-}
-
-- (void)testExchangePublicToken {
-  [_plaid setMockedResponse:@{ @"access_token" : @"test_access_token" }];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid exchangePublicToken:@"public_token" completion:^(id response, NSError *error) {
-    strongify(self);
-    XCTAssertNotNil(response[@"access_token"]);
-    [expectation fulfill];
-  }];
-  [self waitForTestExpectations];
-}
-
-- (void)testGetBalance {
-  [_plaid setMockedResponse:[_plaid exampleConnectResponse]];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid getBalanceWithAccessToken:kTestAccessToken completion:^(id response, NSError *error) {
-    strongify(self);
-    XCTAssertNotNil(response[@"accounts"]);
-    XCTAssertNotNil(response[@"transactions"]);
-    [expectation fulfill];
-  }];
-  [self waitForTestExpectations];
-}
-
-- (void)testUpgradeUser {
-  [_plaid setMockedResponse:[_plaid exampleConnectResponse]];
-  
-  XCTestExpectation *expectation = [self currentExpectation];
-  weakify(self);
-  [_plaid upgradeUserWithAccessToken:kTestAccessToken
-                           upgradeTo:PlaidProductConnect
-                          completion:^(PLDAuthentication *authentication, id response, NSError *error) {
-                            strongify(self);
-                            XCTAssertNotNil(response);
-                            XCTAssertNotNil(authentication);
-                            XCTAssertNotNil(response[@"accounts"]);
-                            XCTAssertNotNil(response[@"transactions"]);
-                            [expectation fulfill];
-                          }];
   [self waitForTestExpectations];
 }
 
